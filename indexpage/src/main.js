91cca35a324e82ab84847770b166dce6ae392b04
tianjiaqi <tianjiaqi@youzan.com>
2018-07-31 20:28:57 +0800

    构建demo

--- indexpage/src/main.js
+++ indexpage/src/main.js
@@ -1,12 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import fullPage from 'vue-fullpage.js';
+import VueFullpage from 'vue-fullpage.js';
 import App from './App';
 
 Vue.config.productionTip = false;
 
-Vue.component('full-page', fullPage.default);
+Vue.use(VueFullpage);
 
 /* eslint-disable no-new */
 new Vue({
